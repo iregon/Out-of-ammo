0d0033bcb05985d87be449626ae4908718588ea9
iregon <0iregon0@gmail.com>
2017-12-16 22:20:59 +0100

    wave spawner

---
+++ Out of ammo/Assets/Scripts/Main/WaveSpawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour {
+
+    public Transform enemyPrefab;
+
+    public Transform spawnPoint;
+
+    public float timeBetweenWaves = 5f;
+    private float countdown = 2f;
+
+    private int waveNumber = 0;
+	
+	// Update is called once per frame
+	void Update () {
+		if(countdown <= 0f)
+        {
+            StartCoroutine(spawnWave());
+            countdown = timeBetweenWaves;
+        }
+
+        countdown -= Time.deltaTime;
+	}
+
+    IEnumerator spawnWave()
+    {
+        waveNumber++;
+        for (int i = 0; i< waveNumber; i++)
+        {
+            spawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    void spawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
